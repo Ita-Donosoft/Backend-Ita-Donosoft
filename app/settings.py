f777b49c41bfed41f58e24f926c2339c7211dbb1
JimmyCamus <jeremy.camus.varela@gmail.com>
2022-06-13 23:26:51 -0400

    feat: config to work with rf tokens

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'core'
+    'core',
+    'auth_app',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
