5742ab27c34b2506adf83e476ac8fb599e121244
SayuriNubi <haylin.perez@alumnos.ucn.cl>
2022-06-20 13:00:53 -0400

    doc: the documentation of add_employee was created.

--- app_secretary/views/add_employee.py
+++ app_secretary/views/add_employee.py
@@ -9,6 +9,16 @@ class AddEmployee(APIView):
     serializer_class = user_serializers.CreateUserSerializer
 
     def post(self, request):
+        """this endpoint is for add an employee
+
+        Args:
+            request (class 'rest_framework.request.Request'): 
+                This class adds the data and information of the request.
+
+        Returns:
+            Response: If the data is not correct it returns an error. 
+            In the contrary case it returns the data of the employee.
+        """
         new_employee = self.serializer_class(data=request.data)
         if not new_employee.is_valid():
             return Response({
