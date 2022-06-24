86435edaf24ccfedb4065857bc330a45a338119c
JimmyCamus <jeremy.camus.varela@gmail.com>
2022-06-24 16:20:51 -0400

    feat: in_service field added to user model in core app

--- core/models/user.py
+++ core/models/user.py
@@ -13,6 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.IntegerField()
     birth_date = models.DateField()
     profession = models.CharField(max_length=50, null=True)
+    in_service = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
