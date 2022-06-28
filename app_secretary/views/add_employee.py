b40a27e07f13da31122642cb7b69cea227b467c3
SayuriNubi <haylin.perez@alumnos.ucn.cl>
2022-06-27 21:35:27 -0400

    feat: The employee contract request was created in add employee - app secretary

--- app_secretary/views/add_employee.py
+++ app_secretary/views/add_employee.py
@@ -1,13 +1,15 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
-from core.serializers import user_serializers
+from core.serializers import user_serializers, request_serializer
 from rest_framework.permissions import IsAuthenticated
+
 
 class AddEmployee(ObtainAuthToken):
     permission_classes = (IsAuthenticated,)
     serializer_class = user_serializers.CreateUserSerializer
     user_serializer_class = user_serializers.UserSerializer
+    request_serializer_class = request_serializer.CreateRequestSerializer
 
     def post(self, request):
         """this endpoint is for add an employee
@@ -25,7 +27,7 @@ class AddEmployee(ObtainAuthToken):
         """
 
         serialized_user = self.user_serializer_class(request.user)
-        if serialized_user.data['role'] != 2: #secretary
+        if serialized_user.data['role'] != 2:  # secretary
             return Response({
                 'error': 'The user must be a secretary'
             }, status=status.HTTP_403_FORBIDDEN
@@ -38,20 +40,35 @@ class AddEmployee(ObtainAuthToken):
             }, status=status.HTTP_400_BAD_REQUEST
             )
 
-        
         if(self.__verify_rut(new_employee.validated_data['rut'])):
             return Response({
                 'error': 'The rut format is wrong. Use the rut without symbols. If the check digit is K, use it in lower case'
             }, status=status.HTTP_400_BAD_REQUEST
             )
-
         new_employee.save()
+
+        self.create_new_contract(new_employee)
+
         return Response({
             'data': new_employee.data
         }, status=status.HTTP_201_CREATED
         )
 
-    
+    def create_new_contract(self, new_employee):
+        """This function is for create a new contract for an employee
+
+        Args:
+            new_employee (CreateUserSerializer): This is the new employee
+        """
+        new_request = self.request_serializer_class(data={
+            'type': 0,
+            'reason':  'new contract',
+            'employee': new_employee.data['id']
+        }
+        )
+        new_request.is_valid()
+        new_request.save()
+
     def __verify_rut(self, rut):
         """This function verify the format of the RUT.
 
@@ -67,4 +84,3 @@ class AddEmployee(ObtainAuthToken):
         rut_check_digit = rut[-1] == 'k' or rut[-1].isnumeric()
 
         return not rut_only_number or (rut_only_number and not rut_check_digit)
-        
