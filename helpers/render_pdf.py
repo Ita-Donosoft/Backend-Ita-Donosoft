e6589648ae68d5c2b3bf81d444d705b60f2e897a
SayuriNubi <haylin.perez@alumnos.ucn.cl>
2022-07-13 21:58:15 -0400

    doc: the documentation of the function to render a html to pdf in helpers folder was created.

--- helpers/render_pdf.py
+++ helpers/render_pdf.py
@@ -1,12 +1,23 @@
 from io import BytesIO
-from re import template
-from unittest import result
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 
 
 def render_html_to_pdf(template_src, data={}):
+    """This function renders a template of html to pdf.
+
+    Args:
+        template_src (str):
+            This var is the route of the template.
+        data (dict, optional): 
+            This is the data to incorporate in the template. Defaults to {}.
+
+    Returns:
+        HttpResponse: 
+            If there is no error it generates a http response with the PDF document.
+            If there is an error it returns None.
+    """
     template = get_template(template_src)
     html = template.render(data)
     result = BytesIO()
